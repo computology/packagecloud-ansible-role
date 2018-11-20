0f3de0247e6956b042583b49956e683a2f1237e5
Julio Capote <jcapote@gmail.com>
2018-11-19 20:10:28 -0500

    specs and fix for /gpgkey

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,3 +1,19 @@
-describe package('curl')   do
+describe package('apt-transport-https') do
   it { should be_installed }
+end
+
+describe package('curl') do
+  it { should be_installed }
+end
+
+describe package('debian-archive-keyring') do
+  it { should be_installed }
+end
+
+describe file('/etc/apt/sources.list.d/capotej_apt-cacher-ng.list') do
+  it { should be_file }
+end
+
+describe command('apt-key list') do
+   its('stdout') { should include ("capotej/apt-cacher-ng") }
 end
